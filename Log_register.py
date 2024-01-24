d19dc5bc77c578e2374ae121f15dbc091f7502de
Maryna Karlovska <65891455+kamarina@users.noreply.github.com>
2024-01-24 20:20:07 +0000

    Add files via upload

---
+++ Log_register.py
@@ -0,0 +1,71 @@
+"""Check if passwords match"""
+
+
+def confirm(new_password):
+    while True:
+        confirm_password = input("Confirm new password: ")
+        if confirm_password != new_password:
+            print("Passwords do not match, please try again. ")
+        else:
+            return confirm_password
+
+
+username_password = {}
+
+
+def main():
+    with open("user.txt", "r") as in_file:
+        for line in in_file:
+            username, password = line.strip().split(";")
+            username_password[username] = password
+
+    while True:
+        choice = input("Would you like to log in(1) or register user(2)? ")
+        if choice == "1":
+            log_in()
+            return choice
+        elif choice == "2":
+            register_user()
+            return choice
+        else:
+            raise ValueError("Invalid choice. Please, enter '1' or '2'. ")
+
+
+""" Register new user """
+
+
+def register_user():
+    print("\n\33[1mREGISTER USER\33[0m")
+    new_username = input("New username:")
+    new_password = input("New password: ")
+    confirm_password = confirm(new_password)
+
+    if confirm_password == new_password:
+        print("New user added! ")
+        username_password[new_username] = new_password
+
+        with open("user.txt", "a") as out_file:
+            out_file.write(f"{new_username};{new_password}\n")
+
+
+"""Log in"""
+
+
+def log_in():
+    global username_password
+    logged_in = False
+    while not logged_in:
+        print("\n\33[1mLOG IN\33[0m")
+        curr_user = input("Username: ")
+        curr_pass = input("Password: ")
+
+        if curr_user in username_password.keys() and username_password[curr_user] == curr_pass:
+            print("Login Successful!")
+            logged_in = True
+        else:
+            print("Login Failed! Try again.")
+
+
+action = main()
+
+"""Tic Tac Toe"""
